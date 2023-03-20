1c4dbec4ecd0e288a83a97f182e3cd292c8eac15
client <client@DESKTOP-76DBPD2>
2023-03-20 21:10:56 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Vehicle Owner");
+		System.out.println("2. Delete Vehicle Owner");
+		System.out.println("3. Edit Vehicle Owner");
+		System.out.println("4. View Vehicle Owner");
+		System.out.println("5. Exit");
+	}
+
+}
